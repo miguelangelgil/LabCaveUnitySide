828da338991eec586c4989af549a456609af7b73
MAGMa <36223494+miguelangelgil@users.noreply.github.com>
2022-06-26 11:14:58 +0200

    done popup when we get an error from node

--- LabCaveTest/Assets/Scripts/RequestServer.cs
+++ LabCaveTest/Assets/Scripts/RequestServer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
 namespace ServerTool 
@@ -22,13 +23,50 @@ namespace ServerTool
 
             if (uwr.isNetworkError)
             {
-                Debug.LogError("Error While Sending: " + uwr.error);
+                Debug.Log("Borosa : " + uwr.error);
+                PopupWindow.Show( new Rect(new Vector2(Screen.width/2,Screen.height/2),new Vector2(200,50)), new PopupExample(uwr.error));
+              
             }
             else
             {
-                Debug.Log("Received: " + uwr.downloadHandler.text);
-                callback.Invoke(uwr.downloadHandler.text);
+                if (uwr.downloadHandler.text.Contains("Error"))
+                {
+                    PopupWindow.Show(new Rect(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(200, 250)), new PopupExample(uwr.error, uwr.downloadHandler.text));
+                }
+                else 
+                {
+                    Debug.Log("Received: " + uwr.downloadHandler.text);
+                    callback.Invoke(uwr.downloadHandler.text);
+                }
+               
             }
         }
     }
+    internal class PopupExample : PopupWindowContent
+    {
+        private string message;
+        private string extrainfo;
+        /// <summary>
+        /// Popup constructor
+        /// </summary>
+        /// <param name="error">server error</param>
+        /// <param name="info">server info about the error</param>
+        public PopupExample(string error, string info="") 
+        {
+            message = error;
+            extrainfo = info;
+        }
+        /// <summary>
+        /// Contains the server error info 
+        /// </summary>
+        /// <param name="rect">Position and size of the popup</param>
+        public override void OnGUI(Rect rect)
+        {
+            GUILayout.Label("Server Error", EditorStyles.boldLabel);
+            GUILayout.TextArea(message);
+            GUILayout.TextArea(extrainfo);
+
+        }
+    }
+
 }
